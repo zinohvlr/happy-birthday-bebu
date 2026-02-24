c7e8877f5684dd3e02cc6ebbc1591f469a046cfe
zinohvlr <santiagojoshlaurence@gmail.com>
2026-02-25 00:49:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMessage() {
+    document.getElementById("message").style.display = "block";
+}
+
+function playMusic() {
+    const song = document.getElementById("song");
+    song.play();
+}
+
+function showVideo() {
+    document.getElementById("video-container").style.display = "block";
+}
